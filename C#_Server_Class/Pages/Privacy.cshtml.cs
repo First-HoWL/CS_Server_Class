2a78ad6e3eea72206f0b1ac05748ebd3e49cadfb
First-HoWL <154261106+First-HoWL@users.noreply.github.com>
2026-02-02 19:00:58 +0200

    Добавьте файлы проекта.

---
+++ C#_Server_Class/Pages/Privacy.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace C__Server_Class.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+
+}
