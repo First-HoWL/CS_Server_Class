8b55ba0f8b8ed14531b5e3a15eb87f0349e0c90a
First-HoWL <154261106+First-HoWL@users.noreply.github.com>
2026-02-03 18:42:45 +0200

    Class Work 02.03, Start Work with headers and fetch

--- C#_Server_Class/Pages/Random.cshtml.cs
+++ C#_Server_Class/Pages/Random.cshtml.cs
@@ -6,56 +6,21 @@ using System.Text.Json;
 
 namespace C__Server_Class.Pages
 {
-    public class ApiDice
-    {
-        public string input { get; set; }
-        public int result { get; set; }
-        public string details { get; set; }
-        public string code { get; set; }
-        public string illustration { get; set; }
-        public long timestamp { get; set; }
-        public long x { get; set; }
-
-
-    }
+    //[IgnoreAntiforgeryToken]
     public class RandomModel : PageModel
     {
         static readonly Random random = new Random();
         public int RandomNumber { get; private set; }
-        public string Dice { get; private set; }
-        public int Result { get; private set; }
-        public async Task OnGet()
+        public void OnGet()
         {
-            using var client = new HttpClient();
-
-            try
+            RandomNumber = random.Next(1, 101);
+        }
+        public IActionResult OnPost()
+        {
+            return new JsonResult(new
             {
-                var response = await client.GetAsync("https://rolz.org/api/?1d20.json");
-
-                response.EnsureSuccessStatusCode();
-
-                var responseBody = await response.Content.ReadAsStringAsync();
-
-                Console.WriteLine("CONTENT TYPE: " + response.Content.Headers.ContentType);
-                Console.WriteLine(responseBody);
-
-                if (!responseBody.TrimStart().StartsWith("{"))
-                {
-                    Console.WriteLine("Это не JSON!");
-                    return;
-                }
-
-                var ressp = JsonSerializer.Deserialize<ApiDice>(responseBody);
-
-                Dice = ressp?.illustration;
-                Result = ressp.result;
-                Console.WriteLine(Dice);
-
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine($"Error: {e.Message}");
-            }
+                number = random.Next(1, 101)
+            });
         }
     }
 }
